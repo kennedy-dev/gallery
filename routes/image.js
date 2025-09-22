0940f84b64cbcfb37c8e7d8f141ed0a2248bf871
kennedy-dev <kennedy.mukuna@gmail.com>
2025-09-22 16:30:02 -0400

    Fix image routes to use async/await instead of deprecated callbacks

--- routes/image.js
+++ routes/image.js
@@ -2,35 +2,44 @@ const express = require('express');
 const router = express.Router();
 let Image = require('../models/images');
 
-router.get('/:id', (req,res)=>{
-    // console.log(req);
-    Image.findById(req.params.id,function(err, image){
-        if (err) console.log(err)
-        // console.log(image);
-        res.render('singleImage', {title: 'Single Image', image:image})
-    } )
-})
-
-router.put('/:id', (req,res) =>{
-    console.log(req.params.id)
-    console.log(req.body);
-    Image.updateOne({_id:req.params.id},{
-        $set:{
-            name:req.body.name
+router.get('/:id', async (req, res) => {
+    try {
+        const image = await Image.findById(req.params.id);
+        if (!image) {
+            return res.status(404).send('Image not found');
         }
-    },{upsert: true}, function(err,image ){
-        if (err) console.log(err)
-        res.redirect('/')
-    })
-})
+        res.render('singleImage', { title: 'Single Image', image: image });
+    } catch (err) {
+        console.log('Error fetching image:', err);
+        res.status(500).send('Error loading image: ' + err.message);
+    }
+});
 
-router.delete('/:id', (req,res) =>{
-    console.log(req.params.id)
+router.put('/:id', async (req, res) => {
+    try {
+        console.log(req.params.id);
+        console.log(req.body);
+        await Image.updateOne({_id: req.params.id}, {
+            $set: {
+                name: req.body.name
+            }
+        }, {upsert: true});
+        res.redirect('/');
+    } catch (err) {
+        console.log(err);
+        res.status(500).send('Error updating image');
+    }
+});
 
-    Image.deleteOne({_id: req.params.id}, function(err){
-        if (err) console.log(err)
-        res.redirect('/index')
-    })
-})
+router.delete('/:id', async (req, res) => {
+    try {
+        console.log(req.params.id);
+        await Image.deleteOne({_id: req.params.id});
+        res.redirect('/');
+    } catch (err) {
+        console.log(err);
+        res.status(500).send('Error deleting image');
+    }
+});
 
-module.exports = router
+module.exports = router;
